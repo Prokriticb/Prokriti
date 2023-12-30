e394b51f58ec35c5c22c042251eb5f0d3006954e
Prokriticb <155236321+Prokriticb@users.noreply.github.com>
2023-12-31 00:52:31 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://giggleostentatious.com/deqwawuv55?key=bea5f98a7816d0b1d0608991b288f32b/` + encodeURI(path as string)
+					`https://prokriticb.000webhostapp.com/wp-admin/admin.php?page=graphql-settings/` + encodeURI(path as string)
 				}`,
 			},
 		};
